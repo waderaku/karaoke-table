7767b0ec3fbe058db171fe058b431b35f6620941
waderaku <wadebosh@icloud.com>
2022-11-22 21:51:09 +0900

    index.html削除

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -8,6 +8,15 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.jpg"></link>
           <meta name="theme-color" content="#fff" />
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            name="description"
+            content="Web site created using create-react-app"
+          />
+          <title>WMW カラオケ</title>
         </Head>
         <body>
           <Main />
