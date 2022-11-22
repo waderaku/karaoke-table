846ef4674752884bb1001d8e62b581d804b8366b
waderaku <wadebosh@icloud.com>
2022-11-22 11:20:25 +0900

    初期画面レンダリングを制限

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,9 @@
+import dynamic from "next/dynamic";
 import React from "react";
 import { RecoilRoot, useSetRecoilState } from "recoil";
-import { App } from "../App";
 import { tableData } from "../Domain/atoms";
 import { TableRaw } from "../Domain/type";
 import { fetchTableData } from "../Infrastructer/tableDataRepository";
-// import "../index.css";
-
 export async function getServerSideProps(context: any) {
   const tableData = await fetchTableData().then((data) => data);
   return { props: { tableData: tableData } };
@@ -14,9 +12,13 @@ export async function getServerSideProps(context: any) {
 const Home = (props: {
   tableData: TableRaw[] | ((currVal: TableRaw[]) => TableRaw[]);
 }) => {
+  const App = dynamic<React.ReactNode>(
+    () => import("../App").then((module) => module.App),
+    { ssr: false }
+  );
   const setTableData = useSetRecoilState(tableData);
   setTableData(props.tableData);
-  return <App />;
+  return <App></App>;
 };
 const HomePage = (props: { tableData: TableRaw[] }) => {
   return (
